8e4eeffa32c3c4cad7a325e157d9feab03206e57
Magnus <nharnharyhaw984@gmail.com>
2024-05-21 20:33:28 +0000

    Testing files added

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def list_state():
+    state_objects = []
+    for state in storage.all(State).values():
+        state_objects.append(state)
+        state_objects = sorted(state_objects, key=lambda state: state.name)
+    return render_template(
+            "7-states_list.html", state_objects=state_objects
+            )
+
+
+@app.teardown_appcontext
+def remove_session():
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
